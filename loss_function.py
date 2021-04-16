d4be27b11e5f9d1ef08536c901a2ebd080c8ff47
cmzuo11 <cmzuo13@163.com>
2021-04-16 15:59:43 +0800

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 29 19:27:03 2019
-
 @author: chunmanzuo
 """
 
 import torch
 from torch.nn import functional as F
-
 import numpy as np
 import math
 import matplotlib
-matplotlib.use('agg')
-from matplotlib import pyplot as plt
-import seaborn as sns
-
-
 
 def binary_cross_entropy(recon_x, x):
-	
 	return - torch.sum(x * torch.log(recon_x + 1e-8) + (1 - x) * torch.log(1 - recon_x + 1e-8), dim=1)
-
 
 def log_zinb_positive(x, mu, theta, pi, eps=1e-8):
 
@@ -51,27 +41,6 @@ def log_zinb_positive(x, mu, theta, pi, eps=1e-8):
 	mul_case_non_zero = torch.mul( (x > eps).type(torch.float32), case_non_zero )
 
 	res = mul_case_zero + mul_case_non_zero
-
-	return - torch.sum( res, dim = 1 )
-
-def log_nb_positive(x, mu, theta, eps=1e-8):
-	
-	x = x.float()
-	
-	if theta.ndimension() == 1:
-		theta = theta.view(
-			1, theta.size(0)
-		)  # In this case, we reshape theta for broadcasting
-
-	log_theta_mu_eps = torch.log(theta + mu + eps)
-
-	res = (
-		theta * (torch.log(theta + eps) - log_theta_mu_eps)
-		+ x * (torch.log(mu + eps) - log_theta_mu_eps)
-		+ torch.lgamma(x + theta)
-		- torch.lgamma(theta)
-		- torch.lgamma(x + 1)
-	)
 
 	return - torch.sum( res, dim = 1 )
 
@@ -107,17 +76,6 @@ def poisson_loss(y_true, y_pred):
 
 	return  torch.sum( ret, dim=1 )
 
-def compute_kernel(x, y):
-	x_size = x.size(0)
-	y_size = y.size(0)
-	dim = x.size(1)
-	x = x.unsqueeze(1) 
-	y = y.unsqueeze(0) 
-	tiled_x = x.expand(x_size, y_size, dim)
-	tiled_y = y.expand(x_size, y_size, dim)
-	kernel_input = (tiled_x - tiled_y).pow(2).mean(2)/float(dim)
-
-	return torch.exp(-kernel_input) # (x_size, y_size)
 
 def GMM_loss(gamma, c_params, z_params):
 	"""
@@ -148,83 +106,3 @@ def GMM_loss(gamma, c_params, z_params):
 	kld = -logpzc - logpc + qentropy + logqcx
 
 	return  kld
-
-def compute_mmd(x, y):
-	x = x.float()
-	y = y.float()
-	x_kernel = compute_kernel(x, x)
-	y_kernel = compute_kernel(y, y)
-	xy_kernel = compute_kernel(x, y)
-	mmd = x_kernel.mean() + y_kernel.mean() - 2*xy_kernel.mean()
-
-	return mmd
-
-def plot_embedding(X, labels, classes=None, method='PCA', cmap='tab20', figsize=(4, 4), markersize=4, marker=None,
-				   return_emb=False, save=False, save_emb=False, show_legend=True, show_axis_label=True, **legend_params):
-
-	if marker is not None:
-		X = np.concatenate([X, marker], axis=0)
-
-	N = len(labels)
-
-	if X.shape[1] != 2:
-
-		if method == 'tSNE':
-			from sklearn.manifold import TSNE
-			X = TSNE(n_components=2, random_state=200).fit_transform(X)
-
-		if method == 'UMAP':
-			from umap import UMAP
-			X = UMAP(n_neighbors=30, min_dist=0.3, metric='correlation').fit_transform(X)
-
-		if method == 'PCA':
-			from sklearn.decomposition import PCA
-			X = PCA(n_components=2, random_state=200).fit_transform(X)
-		
-	plt.figure(figsize=figsize)
-	if classes is None:
-		classes = np.unique(labels)
-
-	if cmap is not None:
-		cmap = cmap
-	elif len(classes) <= 10:
-		cmap = 'tab10'
-	elif len(classes) <= 20:
-		cmap = 'tab20'
-	else:
-		cmap = 'husl'
-	colors = sns.color_palette(cmap, n_colors=len(classes))
-		
-	for i, c in enumerate(classes):
-		plt.scatter(X[:N][labels==c, 0], X[:N][labels==c, 1], s=markersize, color=colors[i], label=c)
-
-	if marker is not None:
-		plt.scatter(X[N:, 0], X[N:, 1], s=10*markersize, color='black', marker='*')
-	#     plt.axis("off")
-	
-	legend_params_ = {'loc': 'center left',
-					 'bbox_to_anchor':(1.0, 0.45),
-					 'fontsize': 10,
-					 'ncol': 1,
-					 'frameon': False,
-					 'markerscale': 1.5
-					}
-	legend_params_.update(**legend_params)
-	if show_legend:
-		plt.legend(**legend_params_)
-	sns.despine(offset=10, trim=True)
-	if show_axis_label:
-		plt.xlabel(method+' dim 1', fontsize=12)
-		plt.ylabel(method+' dim 2', fontsize=12)
-
-	if save:
-		plt.savefig(save, format='pdf', bbox_inches='tight')
-	else:
-		plt.show()
-		
-	if save_emb:
-		np.savetxt(save_emb, X)
-	if return_emb:
-		return X
-
-	plt.clf()
